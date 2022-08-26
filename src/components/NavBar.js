ceb908a64347475bf93e8a4eff015b91c345f92e
ivanleopoldo <ivanleopoldoc@gmail.com>
2022-08-26 11:53:20 +0800

    new ui

---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Home } from '@styled-icons/boxicons-solid';
+import { PersonFill } from '@styled-icons/bootstrap';
+
+const WhiteHome = styled(Home)`
+	color:white;
+	&:hover {
+		color: #26292c;
+	}
+`
+
+const WhitePerson = styled(PersonFill)`
+	color: white;
+	&:hover {
+		color: #26292c;
+	}
+`
+
+const NavBar = () => {
+	return (
+		<div className="absolute lg:bottom-11 md:bottom-9 sm:bottom-7 bottom-5 flex flex-row w-screen justify-center">
+			<div className="border-2 border-neutral-800 rounded-full w-auto px-4 py-3">
+				<ul className="flex flex-row list-none font-white">
+					<li className="w-7 mr-2 ml-1">
+						<a href="https://youtube.com/">
+							<WhiteHome />
+						</a>
+					</li>
+					<li className="w-7 mr-1 ml-2">
+						<WhitePerson />	
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default NavBar;
---
+++ src/components/TopBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../assets/images/dark_logo.svg';
+
+const TopBar = () => {
+	return (
+		<div className="bg-neutral-900 border-b-2 border-neutral-800 rounded-b-3xl">
+			<div className="lg:w-36 md:w-28 sm:w-24 w-16 m-2">
+				<img src={Logo} alt="Logo" />
+			</div>
+		</div>
+	);
+}
+
+export default TopBar;
