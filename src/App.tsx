1631d74c1d7f19c1a9f773faa897b1f14b67c913
ivanleopoldo <ivanleopoldoc@gmail.com>
2022-08-26 09:04:09 +0800

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import Preview from './components/Preview.js';
+import TopBar from './components/TopBar.js';
+import NavBar from './components/NavBar.js';
 
 function App(){
   return (
-		<div className="bg-white flex items-center justify-center h-screen w-screen flex-col">
-			<Preview status={"in development"}/>
+		<div className="bg-neutral-900 h-screen flex flex-col">
+			<TopBar />
+			<div className="absolute flex items-center justify-center h-screen w-screen">
+				<p className="lg:text-5xl md:text-4xl sm:text-3xl text-2xl text-white font-medium tracking-widest">COMING SOON</p>
+			</div>
+			<NavBar />
 		</div>
   );
 }
